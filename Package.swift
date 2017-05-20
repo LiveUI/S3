e5411bf73112bfd601e64a5353c232e452b68bdc
JustinM1 <JustinM1@users.noreply.github.com>
2017-05-20 08:21:14 -0400

    Update Package.swift
    update vapor 2 official release

--- Package.swift
+++ Package.swift
@@ -6,6 +6,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/vapor/crypto.git",
                  Version(2,0,0, prereleaseIdentifiers: ["beta"])),
-        .Package(url: "https://github.com/vapor/core.git", Version(2,0,0, prereleaseIdentifiers: ["beta"]))
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2)
     ]
 )
