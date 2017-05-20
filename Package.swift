816c7a06a31a884711f8f5df6ce0ff64736f8c2a
JustinM1 <JustinM1@users.noreply.github.com>
2017-05-20 08:22:47 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,8 +4,7 @@ let package = Package(
     name: "S3SignerAWS",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/vapor/crypto.git",
-                 Version(2,0,0, prereleaseIdentifiers: ["beta"])),
+        .Package(url: "https://github.com/vapor/crypto.git", majorVersion: 2),
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2)
     ]
 )
